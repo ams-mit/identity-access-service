3c27e403f5f52030f62b62acfd056ead9782b88f
Bajithan Sivathasan <247406187+Bajithan@users.noreply.github.com>
2026-09-06 19:52:11 +0530

    chore: initial Spring Boot project scaffold

---
+++ src/test/java/lk/ac/kelaniya/ams/identity_access_service/IdentityAccessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.kelaniya.ams.identity_access_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityAccessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
